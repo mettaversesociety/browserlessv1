07e36a24f5fdd5489af2234d37e01a0975e978dc
Edwin Lin <edwinlin@Edwins-MacBook-Air.local>
2023-12-06 00:34:08 -0500

    init

---
+++ src/config.ts
@@ -0,0 +1,276 @@
+import fs from 'fs';
+
+import os from 'os';
+
+import debug from 'debug';
+import _ from 'lodash';
+import untildify from 'untildify';
+
+import { Features, isFeature } from './features';
+import { Feature, HeadlessType } from './types.d';
+
+const { IS_CHROME_FOR_TESTING } = require('../env');
+
+// Required, by default, to make certain API's work
+const REQUIRED_INTERNALS = ['url'];
+const REQUIRED_EXTERNALS = ['lighthouse', 'node-pdftk', 'sharp'];
+
+const getDebug = () => {
+  if (typeof process.env.DEBUG !== 'undefined') {
+    return process.env.DEBUG;
+  }
+
+  if (process.env.CI) {
+    return process.env.DEBUG;
+  }
+
+  process.env.DEBUG = 'browserless*';
+  debug.enable(process.env.DEBUG);
+  return process.env.DEBUG;
+};
+
+const getDisabledFeatures = () => {
+  const disabledFeatures: Feature[] = parseJSONParam(
+    process.env.DISABLED_FEATURES,
+    [],
+  ).map((disabledFeature: string) => {
+    if (isFeature(disabledFeature)) {
+      return disabledFeature as Feature;
+    }
+    throw new Error(
+      `Unsupported feature '${disabledFeature}'. Supported features: [${Object.entries(
+        Features,
+      )
+        .map(([, v]) => v)
+        .join(',')}]`,
+    );
+  });
+  if (
+    !parseJSONParam(process.env.ENABLE_DEBUG_VIEWER, true) &&
+    !disabledFeatures.includes(Features.DEBUG_VIEWER)
+  ) {
+    disabledFeatures.push(Features.DEBUG_VIEWER);
+  }
+  return disabledFeatures;
+};
+
+const parseJSONParam = (
+  param: string | undefined,
+  defaultParam: boolean | string | string[],
+) => {
+  if (param) {
+    try {
+      return JSON.parse(param);
+    } catch (err) {
+      console.warn(`Couldn't parse parameter: "${param}". Saw error "${err}"`);
+      return defaultParam;
+    }
+  }
+  return defaultParam;
+};
+
+const parseNumber = (
+  param: string | undefined,
+  defaultParam: number,
+): number => {
+  const parsed = _.parseInt(param || '');
+
+  if (_.isNaN(parsed)) {
+    return defaultParam;
+  }
+
+  return parsed;
+};
+
+const parseSocketBehavior = (behavior = 'http'): 'http' | 'close' => {
+  if (behavior !== 'http' && behavior !== 'close') {
+    console.warn(
+      `Unknown socket behavior of "${behavior}" passed in, using "http"`,
+    );
+    return 'http';
+  }
+
+  return behavior;
+};
+
+const thirtyMinutes = 30 * 60 * 1000;
+const expandedDir = untildify(process.env.WORKSPACE_DIR || '');
+
+// Timers/Queue/Concurrency
+export const CONNECTION_TIMEOUT: number = parseNumber(
+  process.env.CONNECTION_TIMEOUT,
+  30000,
+);
+export const MAX_CONCURRENT_SESSIONS: number = parseNumber(
+  process.env.MAX_CONCURRENT_SESSIONS,
+  10,
+);
+export const QUEUE_LENGTH: number = parseNumber(
+  process.env.MAX_QUEUE_LENGTH,
+  10,
+);
+export const SINGLE_RUN: boolean = parseJSONParam(
+  process.env.SINGLE_RUN,
+  false,
+);
+
+// Pre-boot/Default Launch Options
+export const CHROME_REFRESH_TIME: number = parseNumber(
+  process.env.CHROME_REFRESH_TIME,
+  thirtyMinutes,
+);
+export const KEEP_ALIVE: boolean = parseJSONParam(
+  process.env.KEEP_ALIVE,
+  false,
+);
+export const DEFAULT_BLOCK_ADS: boolean = parseJSONParam(
+  process.env.DEFAULT_BLOCK_ADS,
+  false,
+);
+export const DEFAULT_HEADLESS: HeadlessType = parseJSONParam(
+  process.env.DEFAULT_HEADLESS,
+  IS_CHROME_FOR_TESTING ? 'new' : true,
+);
+export const DEFAULT_LAUNCH_ARGS: string[] = parseJSONParam(
+  process.env.DEFAULT_LAUNCH_ARGS,
+  [],
+);
+export const DEFAULT_IGNORE_DEFAULT_ARGS: boolean = parseJSONParam(
+  process.env.DEFAULT_IGNORE_DEFAULT_ARGS,
+  false,
+);
+export const DEFAULT_IGNORE_HTTPS_ERRORS: boolean = parseJSONParam(
+  process.env.DEFAULT_IGNORE_HTTPS_ERRORS,
+  false,
+);
+export const DEFAULT_DUMPIO: boolean = parseJSONParam(
+  process.env.DEFAULT_DUMPIO,
+  false,
+);
+export const DEFAULT_STEALTH: boolean = parseJSONParam(
+  process.env.DEFAULT_STEALTH,
+  false,
+);
+export const DEFAULT_USER_DATA_DIR: string | undefined = process.env
+  .DEFAULT_USER_DATA_DIR
+  ? untildify(process.env.DEFAULT_USER_DATA_DIR)
+  : undefined;
+export const PREBOOT_CHROME: boolean = parseJSONParam(
+  process.env.PREBOOT_CHROME,
+  false,
+);
+export const PREBOOT_QUANTITY: number = parseNumber(
+  process.env.PREBOOT_QUANTITY,
+  0,
+);
+export const PRINT_GET_STARTED_LINKS: boolean = parseJSONParam(
+  process.env.PRINT_GET_STARTED_LINKS,
+  true,
+);
+export const PRINT_NETWORK_INFO: boolean = parseJSONParam(
+  process.env.PRINT_NETWORK_INFO,
+  true,
+);
+
+// Security and accessibility
+export const DEBUG: string | undefined = getDebug();
+export const DISABLED_FEATURES: Feature[] = getDisabledFeatures();
+export const ENABLE_CORS: boolean = parseJSONParam(
+  process.env.ENABLE_CORS,
+  false,
+);
+export const ENABLE_API_GET: boolean = parseJSONParam(
+  process.env.ENABLE_API_GET,
+  false,
+);
+export const TOKEN: string | null = process.env.TOKEN || null;
+export const ENABLE_HEAP_DUMP: boolean = parseJSONParam(
+  process.env.ENABLE_HEAP_DUMP,
+  false,
+);
+export const ALLOW_FILE_PROTOCOL: boolean = parseJSONParam(
+  process.env.ALLOW_FILE_PROTOCOL,
+  false,
+);
+
+// Puppeteer behavior
+export const DISABLE_AUTO_SET_DOWNLOAD_BEHAVIOR = parseJSONParam(
+  process.env.DISABLE_AUTO_SET_DOWNLOAD_BEHAVIOR,
+  false,
+);
+export const FUNCTION_BUILT_INS: string[] = parseJSONParam(
+  process.env.FUNCTION_BUILT_INS,
+  REQUIRED_INTERNALS,
+);
+export const FUNCTION_ENV_VARS: string[] = parseJSONParam(
+  process.env.FUNCTION_ENV_VARS,
+  [],
+);
+export const FUNCTION_ENABLE_INCOGNITO_MODE: boolean = parseJSONParam(
+  process.env.FUNCTION_ENABLE_INCOGNITO_MODE,
+  false,
+);
+export const FUNCTION_EXTERNALS: string[] = parseJSONParam(
+  process.env.FUNCTION_EXTERNALS,
+  REQUIRED_EXTERNALS,
+);
+export const WORKSPACE_DIR: string = fs.existsSync(expandedDir)
+  ? expandedDir
+  : os.tmpdir();
+export const WORKSPACE_DELETE_EXPIRED: boolean = parseJSONParam(
+  process.env.WORKSPACE_DELETE_EXPIRED,
+  false,
+);
+export const WORKSPACE_EXPIRE_DAYS: number = parseNumber(
+  process.env.WORKSPACE_EXPIRE_DAYS,
+  30,
+);
+
+// Web-hooks
+export const FAILED_HEALTH_URL: string | null =
+  process.env.FAILED_HEALTH_URL || null;
+export const QUEUE_ALERT_URL: string | null =
+  process.env.QUEUE_ALERT_URL || null;
+export const REJECT_ALERT_URL: string | null =
+  process.env.REJECT_ALERT_URL || null;
+export const TIMEOUT_ALERT_URL: string | null =
+  process.env.TIMEOUT_ALERT_URL || null;
+export const ERROR_ALERT_URL: string | null =
+  process.env.ERROR_ALERT_URL || null;
+export const SESSION_CHECK_FAIL_URL: string | null =
+  process.env.SESSION_CHECK_FAIL_URL || null;
+
+// Health
+export const PRE_REQUEST_HEALTH_CHECK: boolean = parseJSONParam(
+  process.env.PRE_REQUEST_HEALTH_CHECK,
+  false,
+);
+export const EXIT_ON_HEALTH_FAILURE: boolean = parseJSONParam(
+  process.env.EXIT_ON_HEALTH_FAILURE,
+  false,
+);
+export const MAX_CPU_PERCENT: number = parseNumber(
+  process.env.MAX_CPU_PERCENT,
+  99,
+);
+export const MAX_MEMORY_PERCENT: number = parseNumber(
+  process.env.MAX_MEMORY_PERCENT,
+  99,
+);
+export const METRICS_JSON_PATH: string | null = process.env.METRICS_JSON_PATH
+  ? untildify(process.env.METRICS_JSON_PATH)
+  : null;
+
+// Server Options
+
+// Host and port to bind our server to
+export const HOST: string | undefined = process.env.HOST;
+export const PORT: number = parseNumber(process.env.PORT, 8080);
+export const SOCKET_CLOSE_METHOD = parseSocketBehavior(
+  process.env.SOCKET_CLOSE_METHOD,
+);
+
+// PROXY URL is used for browserless to build appropriate URLs when it's behind a proxy
+// (must be a fully-qualified URL)
+export const PROXY_URL: string | undefined = process.env.PROXY_URL;
+export const MAX_PAYLOAD_SIZE: string = process.env.MAX_PAYLOAD_SIZE || '5mb';
---
+++ src/utils.ts
@@ -0,0 +1,732 @@
+import fs from 'fs';
+import { stat, mkdir } from 'fs/promises';
+import { IncomingMessage } from 'http';
+import net from 'net';
+import os from 'os';
+import path from 'path';
+import url from 'url';
+import util from 'util';
+
+import cookie from 'cookie';
+import dbg from 'debug';
+import express from 'express';
+import ip from 'ip';
+import { Schema } from 'joi';
+import _ from 'lodash';
+
+import { CDPSession, Page } from 'puppeteer';
+
+import rmrf from 'rimraf';
+
+import { DEFAULT_BLOCK_ADS, DEFAULT_STEALTH, WORKSPACE_DIR } from './config';
+import { WEBDRIVER_ROUTE } from './constants';
+
+import {
+  IWebdriverStartHTTP,
+  IWebdriverStartNormalized,
+  IWorkspaceItem,
+  IUpgradeHandler,
+  IRequestHandler,
+  IHTTPRequest,
+  ILaunchOptions,
+  IBrowser,
+  IDevtoolsJSON,
+  ISession,
+} from './types.d';
+
+const { CHROME_BINARY_LOCATION } = require('../env');
+
+const mkdtemp = util.promisify(fs.mkdtemp);
+
+const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const browserlessDataDirPrefix = 'browserless-data-dir-';
+
+export const jsonProtocolPrefix = 'BROWSERLESS';
+export const lstat = util.promisify(fs.lstat);
+export const readdir = util.promisify(fs.readdir);
+export const writeFile = util.promisify(fs.writeFile);
+export const rimraf = util.promisify(rmrf);
+export const getDebug = (level: string) => dbg(`browserless:${level}`);
+export const exists = (path: string) =>
+  stat(path)
+    .then(() => true)
+    .catch(() => false);
+
+const webdriverSessionCloseReg =
+  /^\/webdriver\/session\/((\w+$)|(\w+\/window))/;
+
+const debug = getDebug('system');
+
+const legacyChromeOptionsKey = 'chromeOptions';
+const w3cChromeOptionsKey = 'goog:chromeOptions';
+
+const readFilesRecursive = async (
+  dir: string,
+  results: IWorkspaceItem[] = [],
+) => {
+  const [, parentDir] = dir.split(WORKSPACE_DIR);
+  const workspaceDir = _.chain(parentDir)
+    .split(path.sep)
+    .compact()
+    .head()
+    .value();
+
+  const files = await readdir(dir);
+
+  await Promise.all(
+    files.map(async (file) => {
+      const stats = await lstat(path.join(dir, file));
+
+      if (stats.isDirectory()) {
+        return readFilesRecursive(path.join(dir, file), results);
+      }
+
+      results.push({
+        created: stats.birthtime,
+        isDirectory: stats.isDirectory(),
+        name: file,
+        path: path.join('/workspace', parentDir, file),
+        size: stats.size,
+        workspaceId: workspaceDir || null,
+      });
+
+      return results;
+    }),
+  );
+
+  return results;
+};
+
+export const id = (prepend = '') =>
+  prepend +
+  Array.from({ length: prepend ? 32 - prepend.length : 32 }, () =>
+    characters.charAt(Math.floor(Math.random() * characters.length)),
+  ).join('');
+
+export const buildWorkspaceDir = async (
+  dir: string,
+): Promise<IWorkspaceItem[] | null> => {
+  const hasDownloads = await exists(dir);
+
+  if (!hasDownloads) {
+    return null;
+  }
+
+  return await readFilesRecursive(dir);
+};
+
+export const getBasicAuthToken = (req: IncomingMessage): string | undefined => {
+  const header = req.headers.authorization || '';
+  const username = header.split(/\s+/).pop() || '';
+  const token = Buffer.from(username, 'base64').toString().replace(':', '');
+
+  return token.length ? token : undefined;
+};
+
+export const asyncWsHandler = (handler: IUpgradeHandler) => {
+  return (req: IncomingMessage, socket: net.Socket, head: Buffer) => {
+    Promise.resolve(handler(req, socket, head)).catch((error: Error) => {
+      debug(`Error in WebSocket handler: ${error} ${error.stack}`);
+      socket.write(
+        [
+          'HTTP/1.1 400 Bad Request',
+          'Content-Type: text/plain; charset=UTF-8',
+          'Content-Encoding: UTF-8',
+          'Accept-Ranges: bytes',
+          'Connection: keep-alive',
+          '\r\n',
+          'Bad Request, ' + error.message,
+        ].join('\r\n'),
+      );
+      socket.end();
+    });
+  };
+};
+
+export const asyncWebHandler = (handler: IRequestHandler) => {
+  return (req: express.Request, res: express.Response) => {
+    Promise.resolve(handler(req, res)).catch((error) => {
+      debug(`Error in route handler: ${error}`);
+      res.status(400).send(error.message);
+    });
+  };
+};
+
+export const bodyValidation = (schema: Schema) => {
+  return (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    const header = req.header('content-type');
+    if (header && !header.includes('json')) {
+      return next();
+    }
+
+    const result = schema.validate(req.body);
+
+    if (result.error) {
+      debug(`Malformed incoming request: ${result.error}`);
+      return res.status(400).send(result.error.details);
+    }
+
+    // Allow .defaults to work otherwise
+    // Joi schemas default's won't apply
+    req.body = result.value;
+
+    return next();
+  };
+};
+
+export const queryValidation = (schema: Schema) => {
+  return (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    let inflated: string | null = null;
+
+    if (typeof req.query.body !== 'string') {
+      return res
+        .status(400)
+        .send(
+          `The query-parameter "body" is required, and must be a URL-encoded normalized JSON object.`,
+        );
+    }
+
+    try {
+      inflated = JSON.parse(req.query.body);
+    } catch {
+      inflated = null;
+    }
+
+    if (!inflated) {
+      return res
+        .status(400)
+        .send(
+          `The query-parameter "body" is required, and must be a URL-encoded normalized JSON object.`,
+        );
+    }
+
+    const result = schema.validate(inflated);
+
+    if (result.error) {
+      debug(`Malformed incoming request: ${result.error}`);
+      return res.status(400).send(result.error.details);
+    }
+
+    // Allow .defaults to work otherwise
+    // Joi schemas default's won't apply
+    req.body = result.value;
+
+    return next();
+  };
+};
+
+export const tokenCookieName = 'browserless_token';
+export const codeCookieName = 'browserless_code';
+
+export const isAuthorized = (req: IHTTPRequest, token: string) => {
+  const cookies = cookie.parse(req.headers.cookie || '');
+  const parsedUrl = req.parsed;
+  const authToken =
+    _.get(parsedUrl, 'query.token', null) ||
+    getBasicAuthToken(req) ||
+    cookies[tokenCookieName];
+
+  if (authToken !== token) {
+    return false;
+  }
+
+  return true;
+};
+
+export const fetchJson = (url: string, opts?: any) =>
+  fetch(url, opts).then((res) => {
+    if (!res.ok) {
+      throw res;
+    }
+    return res.json();
+  });
+
+export const generateChromeTarget = () => {
+  return `/devtools/page/${id(jsonProtocolPrefix)}`;
+};
+
+export const sleep = function sleep(time = 0) {
+  return new Promise((r) => {
+    global.setTimeout(r, time);
+  });
+};
+
+const safeParse = (maybeJson: any) => {
+  try {
+    return JSON.parse(maybeJson);
+  } catch {
+    return null;
+  }
+};
+
+export const normalizeWebdriverStart = async (
+  req: IncomingMessage,
+): Promise<IWebdriverStartNormalized> => {
+  const body = await readRequestBody(req);
+  const parsed = safeParse(body);
+
+  let browserlessDataDir: string | null = null;
+
+  // First, convert legacy chrome options to W3C spec
+  if (_.has(parsed, ['desiredCapabilities', legacyChromeOptionsKey])) {
+    parsed.desiredCapabilities[w3cChromeOptionsKey] = _.cloneDeep(
+      parsed.desiredCapabilities[legacyChromeOptionsKey],
+    );
+    delete parsed.desiredCapabilities[legacyChromeOptionsKey];
+  }
+
+  if (_.has(parsed, ['capabilities', 'alwaysMatch', legacyChromeOptionsKey])) {
+    parsed.capabilities.alwaysMatch[w3cChromeOptionsKey] = _.cloneDeep(
+      parsed.capabilities.alwaysMatch[legacyChromeOptionsKey] ||
+        parsed.desiredCapabilities[w3cChromeOptionsKey],
+    );
+    delete parsed.capabilities.alwaysMatch[legacyChromeOptionsKey];
+  }
+
+  if (
+    _.has(parsed, ['capabilities', 'firstMatch']) &&
+    _.some(parsed.capabilities.firstMatch, (opt) => opt[legacyChromeOptionsKey])
+  ) {
+    _.each(parsed.capabilities.firstMatch, (opt) => {
+      if (opt[legacyChromeOptionsKey]) {
+        opt[w3cChromeOptionsKey] = _.cloneDeep(opt[legacyChromeOptionsKey]);
+        delete opt[legacyChromeOptionsKey];
+      }
+    });
+  }
+
+  const capabilities = _.merge(
+    {},
+    parsed?.capabilities?.firstMatch?.['0'],
+    parsed?.capabilities?.alwaysMatch,
+    parsed?.desiredCapabilities,
+  );
+
+  const launchArgs = _.get(
+    capabilities,
+    [w3cChromeOptionsKey, 'args'],
+    [],
+  ) as string[];
+
+  // Set a temp data dir
+  const isUsingTempDataDir = !launchArgs.some((arg: string) =>
+    arg.startsWith('--user-data-dir'),
+  );
+  browserlessDataDir = isUsingTempDataDir ? await getUserDataDir() : null;
+
+  // Set binary path and user-data-dir
+  if (_.has(parsed, ['desiredCapabilities', w3cChromeOptionsKey])) {
+    if (isUsingTempDataDir) {
+      parsed.desiredCapabilities[w3cChromeOptionsKey].args =
+        parsed.desiredCapabilities[w3cChromeOptionsKey].args || [];
+      parsed.desiredCapabilities[w3cChromeOptionsKey].args.push(
+        `--user-data-dir=${browserlessDataDir}`,
+      );
+    }
+    parsed.desiredCapabilities[w3cChromeOptionsKey].binary =
+      CHROME_BINARY_LOCATION;
+  }
+
+  if (_.has(parsed, ['capabilities', 'alwaysMatch', w3cChromeOptionsKey])) {
+    if (isUsingTempDataDir) {
+      parsed.capabilities.alwaysMatch[w3cChromeOptionsKey].args =
+        parsed.capabilities.alwaysMatch[w3cChromeOptionsKey].args || [];
+      parsed.capabilities.alwaysMatch[w3cChromeOptionsKey].args.push(
+        `--user-data-dir=${browserlessDataDir}`,
+      );
+    }
+    parsed.capabilities.alwaysMatch[w3cChromeOptionsKey].binary =
+      CHROME_BINARY_LOCATION;
+  }
+
+  if (
+    _.has(parsed, ['capabilities', 'firstMatch']) &&
+    _.some(parsed.capabilities.firstMatch, (opt) => opt[w3cChromeOptionsKey])
+  ) {
+    _.each(parsed.capabilities.firstMatch, (opt) => {
+      if (opt[w3cChromeOptionsKey]) {
+        if (isUsingTempDataDir) {
+          opt[w3cChromeOptionsKey].args = opt[w3cChromeOptionsKey].args || [];
+          opt[w3cChromeOptionsKey].args.push(
+            `--user-data-dir=${browserlessDataDir}`,
+          );
+        }
+        opt[w3cChromeOptionsKey].binary = CHROME_BINARY_LOCATION;
+      }
+    });
+  }
+
+  const blockAds = !!(
+    capabilities['browserless.blockAds'] ??
+    capabilities['browserless:blockAds'] ??
+    DEFAULT_BLOCK_ADS
+  );
+
+  const stealth = !!(
+    capabilities['browserless.stealth'] ??
+    capabilities['browserless:stealth'] ??
+    DEFAULT_STEALTH
+  );
+
+  const token =
+    capabilities['browserless.token'] ??
+    capabilities['browserless:token'] ??
+    getBasicAuthToken(req);
+
+  const pauseOnConnect = !!(
+    capabilities['browserless.pause'] ?? capabilities['browserless:pause']
+  );
+
+  const trackingId =
+    capabilities['browserless.trackingId'] ??
+    capabilities['browserless:trackingId'] ??
+    null;
+
+  const windowSizeArg = launchArgs.find((arg) => arg.includes('window-size='));
+  const windowSizeParsed =
+    windowSizeArg && windowSizeArg.split('=')[1].split(',');
+  let windowSize: IWebdriverStartNormalized['params']['windowSize'];
+
+  if (Array.isArray(windowSizeParsed)) {
+    const [width, height, deviceScaleFactor] = windowSizeParsed;
+    windowSize = {
+      width: +width,
+      height: +height,
+    };
+    if (deviceScaleFactor) {
+      windowSize.deviceScaleFactor = parseInt(deviceScaleFactor);
+    }
+  }
+
+  return {
+    body: parsed,
+    params: {
+      token,
+      stealth,
+      blockAds,
+      trackingId,
+      pauseOnConnect,
+      windowSize,
+      isUsingTempDataDir,
+      browserlessDataDir,
+    },
+  };
+};
+
+// NOTE, if proxying request elsewhere, you must re-stream the body again
+const readRequestBody = async (req: IncomingMessage): Promise<any> => {
+  return new Promise((resolve) => {
+    const body: Uint8Array[] = [];
+    let hasResolved = false;
+
+    const resolveNow = (results: any) => {
+      if (hasResolved) {
+        return;
+      }
+      hasResolved = true;
+      resolve(results);
+    };
+
+    req
+      .on('data', (chunk) => body.push(chunk))
+      .on('end', () => {
+        const final = Buffer.concat(body).toString();
+        if (hasResolved) {
+          return;
+        }
+        resolveNow(final);
+      })
+      .on('aborted', () => {
+        resolveNow(null);
+      })
+      .on('error', () => {
+        resolveNow(null);
+      });
+  });
+};
+
+export const fnLoader = (fnName: string) =>
+  fs.readFileSync(
+    path.join(__dirname, '..', 'functions', `${fnName}.js`),
+    'utf8',
+  );
+
+export const getUserDataDir = () =>
+  mkdtemp(path.join(os.tmpdir(), browserlessDataDirPrefix));
+
+export const clearBrowserlessDataDirs = () =>
+  rimraf(path.join(os.tmpdir(), `${browserlessDataDirPrefix}*`));
+
+export const mkDataDir = async (path: string) => {
+  if (await exists(path)) {
+    return;
+  }
+  await mkdir(path, { recursive: true });
+};
+
+export const parseRequest = (req: IncomingMessage): IHTTPRequest => {
+  const ret: IHTTPRequest = req as IHTTPRequest;
+  const parsed = url.parse(req.url || '', true);
+
+  ret.parsed = parsed;
+
+  return ret;
+};
+
+// Number = time in MS, undefined = no timeout, null = no timeout set and should use system-default
+export const getTimeoutParam = (
+  req: IHTTPRequest | IWebdriverStartHTTP,
+): number | undefined | null => {
+  const payloadTimer =
+    req.method === 'POST' &&
+    req.url &&
+    req.url.includes('webdriver') &&
+    Object.prototype.hasOwnProperty.call(req, 'body')
+      ? _.get(
+          req,
+          ['body', 'desiredCapabilities', 'browserless.timeout'],
+          null,
+        ) ||
+        _.get(
+          req,
+          ['body', 'capabilities', 'alwaysMatch', 'browserless:timeout'], // Selenium 4.5 > calls
+          null,
+        )
+      : _.get(req, 'parsed.query.timeout', null);
+
+  if (_.isArray(payloadTimer)) {
+    return null;
+  }
+
+  const parsedTimer = _.parseInt(payloadTimer || '');
+
+  if (_.isNaN(parsedTimer)) {
+    return null;
+  }
+
+  if (parsedTimer === -1) {
+    return undefined;
+  }
+
+  if (_.isNumber(parsedTimer)) {
+    return parsedTimer;
+  }
+
+  return null;
+};
+
+export const isWebdriverStart = (req: IncomingMessage) => {
+  return req.method?.toLowerCase() === 'post' && req.url === WEBDRIVER_ROUTE;
+};
+
+export const isWebdriverClose = (req: IncomingMessage) => {
+  return (
+    req.method?.toLowerCase() === 'delete' &&
+    webdriverSessionCloseReg.test(req.url || '')
+  );
+};
+
+export const isWebdriver = (req: IncomingMessage) => {
+  return req.url?.includes(WEBDRIVER_ROUTE);
+};
+
+export const canPreboot = (
+  incoming: ILaunchOptions,
+  defaults: ILaunchOptions,
+) => {
+  if (incoming.playwright) {
+    return false;
+  }
+
+  if (
+    !_.isUndefined(incoming.headless) &&
+    incoming.headless !== defaults.headless
+  ) {
+    return false;
+  }
+
+  if (
+    !_.isUndefined(incoming.args) &&
+    _.difference(incoming.args, defaults.args as string[]).length
+  ) {
+    return false;
+  }
+
+  if (!_.isUndefined(incoming.ignoreDefaultArgs)) {
+    if (
+      typeof incoming.ignoreDefaultArgs !== typeof defaults.ignoreDefaultArgs
+    ) {
+      return false;
+    }
+
+    if (
+      Array.isArray(incoming.ignoreDefaultArgs) &&
+      Array.isArray(defaults.ignoreDefaultArgs)
+    ) {
+      return !_.difference(
+        incoming.ignoreDefaultArgs,
+        defaults.ignoreDefaultArgs,
+      ).length;
+    }
+
+    if (incoming.ignoreDefaultArgs !== defaults.ignoreDefaultArgs) {
+      return false;
+    }
+  }
+
+  if (
+    !_.isUndefined(incoming.userDataDir) &&
+    incoming.userDataDir !== defaults.userDataDir
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+export const dedent = (strings: string | string[], ...values: string[]) => {
+  const raw = Array.isArray(strings) ? strings : [strings];
+
+  let result = '';
+
+  for (let i = 0; i < raw.length; i++) {
+    result += raw[i]
+      // join lines when there is a suppressed newline
+      .replace(/\\\n[ \t]*/g, '')
+      // handle escaped backticks
+      .replace(/\\`/g, '`');
+
+    if (i < values.length) {
+      result += values[i];
+    }
+  }
+
+  // now strip indentation
+  const lines = result.split('\n');
+  let mIndent: number | null = null;
+  lines.forEach((l) => {
+    const m = l.match(/^(\s+)\S+/);
+    if (m) {
+      const indent = m[1].length;
+      if (!mIndent) {
+        // this is the first indented line
+        mIndent = indent;
+      } else {
+        mIndent = Math.min(mIndent, indent);
+      }
+    }
+  });
+
+  if (mIndent !== null) {
+    const m = mIndent;
+    result = lines.map((l) => (l[0] === ' ' ? l.slice(m) : l)).join('\n');
+  }
+
+  return (
+    result
+      // dedent eats leading and trailing whitespace too
+      .trim()
+      // handle escaped newlines at the end to ensure they don't get stripped too
+      .replace(/\\n/g, '\n')
+  );
+};
+
+export const urlJoinPaths = (...paths: string[]) =>
+  paths.map((s) => _.trim(s, '/')).join('/');
+
+export const injectHostIntoSession = (
+  host: URL,
+  browser: IBrowser,
+  session: IDevtoolsJSON,
+): ISession => {
+  const { port } = browser._parsed;
+  const wsEndpoint = browser._wsEndpoint;
+  const isSSL = host.protocol === 'https:';
+
+  if (!port) {
+    throw new Error(`No port found for browser devtools!`);
+  }
+
+  const parsedWebSocketDebuggerUrl = new URL(session.webSocketDebuggerUrl);
+  const parsedWsEndpoint = new URL(wsEndpoint);
+  const parsedDevtoolsFrontendURL = new URL(
+    session.devtoolsFrontendUrl,
+    host.href,
+  );
+
+  // Override the URL primitives to the base host or proxy
+  parsedWebSocketDebuggerUrl.hostname = host.hostname;
+  parsedWebSocketDebuggerUrl.port = host.port;
+  parsedWebSocketDebuggerUrl.protocol = isSSL ? 'wss:' : 'ws:';
+
+  parsedWsEndpoint.hostname = host.hostname;
+  parsedWsEndpoint.port = host.port;
+  parsedWsEndpoint.protocol = isSSL ? 'wss:' : 'ws:';
+
+  // Prepend any base-path of the external URL's
+  if (host.pathname !== '/') {
+    parsedWebSocketDebuggerUrl.pathname = urlJoinPaths(
+      host.pathname,
+      parsedWebSocketDebuggerUrl.pathname,
+    );
+    parsedWsEndpoint.pathname = urlJoinPaths(
+      host.pathname,
+      parsedWsEndpoint.pathname,
+    );
+    parsedDevtoolsFrontendURL.pathname = urlJoinPaths(
+      host.pathname,
+      parsedDevtoolsFrontendURL.pathname,
+    );
+  }
+
+  parsedDevtoolsFrontendURL.search = `?${isSSL ? 'wss' : 'ws'}=${host.host}${
+    parsedWebSocketDebuggerUrl.pathname
+  }`;
+
+  const browserWSEndpoint = parsedWsEndpoint.href;
+  const webSocketDebuggerUrl = parsedWebSocketDebuggerUrl.href;
+  const devtoolsFrontendUrl =
+    parsedDevtoolsFrontendURL.pathname + parsedDevtoolsFrontendURL.search;
+
+  return {
+    ...session,
+    port,
+    browserId: browser._id,
+    trackingId: browser._trackingId,
+    browserWSEndpoint,
+    devtoolsFrontendUrl,
+    webSocketDebuggerUrl,
+  };
+};
+
+export const getCDPClient = (page: Page): CDPSession => {
+  // @ts-ignore using internal CDP client
+  const c = page._client;
+
+  return typeof c === 'function' ? c.call(page) : c;
+};
+
+export const printGetStartedLinks = (debug: dbg.Debugger) => {
+  debug(`
+Get started at\t https://www.browserless.io/docs/start
+Get a license at\t https://www.browserless.io/sign-up?type=commercial
+Get support at\t https://www.browserless.io/contact
+
+Happy coding!
+`);
+};
+
+export const printNetworkInfo = (debug: dbg.Debugger, port: number) => {
+  debug(`\n
+Running on port ${port}
+\tLocalhost\t ws:localhost:${port}
+\tLocal network\t ws:${ip.address()}:${port}`);
+};
